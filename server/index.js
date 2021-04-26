599ad8afe1dc0307ece411b8e0e52d2a29457ec8
chentimothy1 <timothychen1@protonmail.com>
2021-04-26 01:49:27 -0700

    added .env file

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 // every route inside postRoutes will start with posts
 
@@ -16,11 +18,11 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 
-const CONNECTION_URL = 'mongodb+srv://timothychen:timothyspassword@cluster0.ixmty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// const CONNECTION_URL = 'mongodb+srv://timothychen:timothyspassword@cluster0.ixmty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
 // prevents warnings in console
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     // if the call is successful
     .then(() => app.listen(PORT, () => console.log(`Server's running on port: ${PORT}`)))
     // if error
