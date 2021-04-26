d53f0c221f0422fe6453df76a624e9d5358023a9
chentimothy1 <timothychen1@protonmail.com>
2021-04-26 00:16:43 -0700

    added svg background, updated UI of posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Post from './Post/Post';
 import useStyles from './styles';
 import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core';
+
 
 const Posts = () => {
     const classes = useStyles();
@@ -10,12 +12,17 @@ const Posts = () => {
     console.log(posts);
 
     return (
-        <>
-        <h1> POSTS </h1>
-        <Post />
-        <Post />
-
-        </>
+        // if posts.length is something other than 0, everything will be false and the posts will be showed
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {posts.map((post) => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post}/>
+                    </Grid>
+                ))    
+                }
+            </Grid>
+        )
     )
 }
 
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
-import { getPosts, createPost} from '../controllers/posts.js';
+import { getPosts, createPost, updatePost } from '../controllers/posts.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
+//patch updates existing documents
+router.patch('/:id', updatePost)
 
 export default router;
