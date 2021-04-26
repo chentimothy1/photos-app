1061ea4093ba67326874bce1dd67ccdf542aa273
chentimothy1 <timothychen1@protonmail.com>
2021-04-26 16:26:48 -0700

    implemented user backend, started on auth middleware for user permissions in app

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
+
 
 const app = express();
 dotenv.config();
@@ -15,7 +17,11 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+//route for posts
 app.use('/posts', postRoutes);
+
+//route for users
+app.use('/user', userRoutes);
 
 
 // const CONNECTION_URL = 'mongodb+srv://timothychen:timothyspassword@cluster0.ixmty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
---
+++ server/middleware/auth.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+
+
+//clicks the like button -> auth middleware(). if the authmiddleware says he can like it, we send it to the 
+// like middleware 
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const isCustomAuth = token.length < 500;
+        
+        let decodedData;
+        if(token && isCustomAuth) {
+            decodedData = jwt.verify(token, 'test');
+            req.userId = decodedData ?.indexOf;
+        } else {
+            decodedData = jwt.decode(token);
+
+            req.userId = decodedData ?.sub;
+        }
+
+        next();
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default auth;
